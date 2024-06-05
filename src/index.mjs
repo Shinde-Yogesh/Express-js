1a1c4587f534012aa5e82de1ca12bd71c23b29c0
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-06-05 15:13:09 +0530

    get request created with json sample data

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,21 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express();
 
 const PORT =  process.env.PORT ||3000;
+
+app.get("/",(request, response) =>{
+response.send("Hello divinee");
+});
+
+app.get("/api/users",(request, response) =>{
+    response.send([
+        
+        {id: 1, username:"anson" ,displayName:"anson"},
+        {id: 2, username:"Gimmy" ,displayName:"Lord"},
+        {id: 3, username:"Jack" ,displayName:"Jans"},
+    ])
+})
 app.listen (PORT, ()=>{
     console.log(`listening on ${PORT}`);
 });
